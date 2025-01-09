3f5869abd18425ad79c20e82883878475163d638
DynomiteWaffle <dynomitewaffle@protonmail.com>
2025-01-09 14:17:50 -0700

    Started Final Product/Redesign

--- src/main.go
+++ src/main.go
@@ -3,101 +3,81 @@ package main
 import (
 	"image/color"
 	"log"
-	"slices"
-	"strconv"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
-type displayMap struct {
-	m []int
-	w int
-	s int
-	o point
-	p int
+// TODO web wont render
+
+// | 0 - toggle pan/select |
+// 1 - zoom in |
+// 2 - zoom out |
+// 3 - toggle settings |
+var buttons = [4]Button{}
+var barScale = 12
+var panning = false
+
+var (
+	r = color.RGBA{R: 255, G: 0, B: 0, A: 255}
+	g = color.RGBA{R: 0, G: 255, B: 0, A: 255}
+	b = color.RGBA{R: 0, G: 0, B: 255, A: 255}
+)
+
+type Game struct {
 }
-type point struct {
-	x int
-	y int
+
+type Button struct {
+	run       func(toggle bool)
+	toggle    bool
+	togglable bool
+	icon1     *ebiten.Image
+	icon2     ebiten.Image
 }
 
-var pallette []color.Color = slices.Repeat([]color.Color{color.White}, 255)
-
-// TODO convert json hex map to array of ints
-func Hex2Map(input string) []int {
-	var nums []int = []int{}
-	for i := 0; i < len(input); i += 2 {
-		// fmt.Println(input[i : i+2])
-		num, _ := strconv.ParseInt(input[i:i+2], 16, 16)
-		nums = append(nums, int(num))
-
+func (b *Button) Toggle() {
+	if b.toggle {
+		b.toggle = false
+	} else {
+		b.toggle = true
 	}
-	return nums
 }
-
-var Map displayMap = displayMap{
-	m: []int{},
-	w: 3,  //width
-	s: 20, // scale
-	p: 2,  //padding
-}
-var mouseDist point
-var oldMousePos point
-var scrollSpeed int = 10
-var windowScale int
-var touches []ebiten.TouchID
-
-// temp
-var debugText string
-
-type Game struct{}
 
 func (g *Game) Update() error {
-	// window scale
-	var w, _ = ebiten.WindowSize()
-	windowScale = w / 8
-	// move map
-	touches = ebiten.AppendTouchIDs(touches)
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButton2) {
-		mouseDist.x, mouseDist.y = ebiten.CursorPosition()
-		mouseDist.x -= oldMousePos.x
-		mouseDist.y -= oldMousePos.y
-
-	} else if len(touches) > 0 {
-		mouseDist.x, mouseDist.y = ebiten.TouchPosition(touches[0])
-		// second touch can be used for paning/zooming
-		// DEBUG
-		debugText = strconv.Itoa(mouseDist.x) + ":" + strconv.Itoa(mouseDist.y) + "\n" + debugText
-		mouseDist.x -= Map.o.x
-		mouseDist.y -= Map.o.y
-	} else {
-		oldMousePos.x, oldMousePos.y = ebiten.CursorPosition()
-		Map.o.x += mouseDist.x
-		Map.o.y += mouseDist.y
-		mouseDist.x, mouseDist.y = 0, 0
-
-	}
-	// zoom map
-	// TODO zoom from center
-	var _, wheel = ebiten.Wheel()
-	Map.s += int(wheel) * scrollSpeed
-	// TODO translate to old origin
-
-	// TODO mobile touches/pans
+	// TODO click/activate buttons
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, debugText)
-	var scale = Map.s + windowScale
-	// draw map
-	for i := 0; i < len(Map.m); i++ {
-		var Colum int = i % Map.w
-		var Row int = i / Map.w
-		vector.DrawFilledRect(screen, float32(mouseDist.x+Map.o.x+(Colum)*scale), float32(mouseDist.y+Map.o.y+(Row)*scale), float32(scale-Map.p), float32(scale-Map.p), pallette[Map.m[i]], true)
+	var width, height = ebiten.WindowSize()
+	vector.DrawFilledRect(screen, 0, 0, float32(width), float32(height)/float32(barScale), color.White, true)
+	// draw buttons
+	var barHeight float64
+	barHeight = float64(height) / float64(barScale)
+	// distance from left edge, buttons get added to this
+	var dist = 0.0
+	var op = &ebiten.DrawImageOptions{}
+	for b := 0; b < len(buttons); b++ {
+		if !buttons[b].toggle {
+			// scale image
+			var scale = barHeight / float64(buttons[b].icon1.Bounds().Dy())
+			op.GeoM.Scale(float64(scale), float64(scale))
+			op.GeoM.Translate(float64(dist), 0)
+			screen.DrawImage(buttons[b].icon1, op)
+			dist += float64(buttons[b].icon1.Bounds().Dx()) * scale
+			op.GeoM.Reset()
+		} else {
+			// TODO draw icon 2
+			var scale = barHeight / float64(buttons[b].icon2.Bounds().Dy())
+			op.GeoM.Scale(float64(scale), float64(scale))
+			op.GeoM.Translate(float64(dist), 0)
+			screen.DrawImage(&buttons[b].icon2, op)
+			dist += float64(buttons[b].icon2.Bounds().Dx()) * scale
+			op.GeoM.Reset()
+		}
 	}
+	// screen.DrawImage(buttons[0].icon1, op)
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -105,17 +85,19 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	Map.m = Hex2Map("000102010101030100")
-	Map.o.y = 20
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("CheckerWars")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-	// TODO Build Color Pallette
-	pallette[0] = color.RGBA{R: 0, G: 0, B: 0, A: 255}
-	pallette[1] = color.RGBA{R: 255, G: 255, B: 255, A: 255}
-	pallette[2] = color.RGBA{R: 255, A: 255}
-	pallette[3] = color.RGBA{B: 255, A: 255}
-
+	buttons[0].icon1 = ebiten.NewImage(20, 20)
+	buttons[0].icon1.Fill(color.Black)
+	buttons[1].icon1 = ebiten.NewImage(40, 40)
+	buttons[1].icon1.Fill(r)
+	buttons[2].icon1 = ebiten.NewImage(20, 20)
+	buttons[2].icon1.Fill(g)
+	buttons[3].icon1 = ebiten.NewImage(20, 20)
+	buttons[3].icon1.Fill(b)
+	// setup buttons
+	//
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
